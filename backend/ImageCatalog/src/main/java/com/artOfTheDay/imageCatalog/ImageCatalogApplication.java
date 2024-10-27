ef25e8b079bb6a0297e4ce34f94b47e1e90cef03
Vukan Antić <61616630+VukanAntic@users.noreply.github.com>
2024-10-27 17:18:44 +0100

    Init ImageCatalog microservice

---
+++ backend/ImageCatalog/src/main/java/com/artOfTheDay/imageCatalog/ImageCatalogApplication.java
@@ -0,0 +1,13 @@
+package com.artOfTheDay.imageCatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageCatalogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImageCatalogApplication.class, args);
+    }
+
+}
