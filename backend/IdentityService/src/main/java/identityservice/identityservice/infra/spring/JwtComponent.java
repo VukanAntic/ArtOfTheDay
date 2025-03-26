78f4ee6ce9097d1cc3fd54366897ca32429a8282
Vukan Antić <61616630+VukanAntic@users.noreply.github.com>
2025-03-26 14:09:53 +0100

    [IndetityService] jwt wip

---
+++ backend/IdentityService/src/main/java/identityservice/identityservice/infra/spring/JwtComponent.java
@@ -0,0 +1,60 @@
+package identityservice.identityservice.infra.spring;
+
+
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import java.security.Key;
+import java.util.Date;
+
+@Component
+public class JwtComponent {
+
+    @Value("${jwt.secret}")
+    private String secret;
+    private final long ACCESS_TOKEN_EXPIRATION = 15 * 60 * 1000L; // 15 min
+    private final long REFRESH_TOKEN_EXPIRATION = 2 * 30 * 24 * 60 * 60 * 1000L; // 2 months
+
+    private Key getSigningKey() {
+        return Keys.hmacShaKeyFor(secret.getBytes());
+    }
+
+    public String generateAccessToken(String username) {
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + ACCESS_TOKEN_EXPIRATION))
+                .signWith(getSigningKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public String generateRefreshToken(String username) {
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + REFRESH_TOKEN_EXPIRATION))
+                .signWith(getSigningKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parserBuilder().setSigningKey(getSigningKey()).build().parseClaimsJws(token);
+            return true;
+        } catch (JwtException e) {
+            return false;
+        }
+    }
+
+    public String extractUsername(String token) {
+        return Jwts.parserBuilder().setSigningKey(getSigningKey()).build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+
+}
