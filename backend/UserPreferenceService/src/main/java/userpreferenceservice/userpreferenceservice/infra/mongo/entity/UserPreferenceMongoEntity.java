eab0ea81dcb04ad36ea43706c0a8b3166563e7c6
Vukan Antić <61616630+VukanAntic@users.noreply.github.com>
2025-05-06 19:07:02 +0200

    [UserPreferencesService] Added needed preferences to userEntity

--- backend/UserPreferenceService/src/main/java/userpreferenceservice/userpreferenceservice/infra/mongo/entity/UserPreferenceMongoEntity.java
+++ backend/UserPreferenceService/src/main/java/userpreferenceservice/userpreferenceservice/infra/mongo/entity/UserPreferenceMongoEntity.java
@@ -4,6 +4,8 @@ import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.util.Set;
+
 @Document(collection = "user_preferences")
 @AllArgsConstructor
 @NoArgsConstructor
@@ -13,4 +15,6 @@ import org.springframework.data.mongodb.core.mapping.Document;
 public class UserPreferenceMongoEntity {
     @Id
     private String userId;
+    private Set<String> favoriteArtworkIds;
+    private Set<String> favouriteGenreIds;
 }
