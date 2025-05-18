58f77d72f5434f2bcf5ad47991a84b51357fce09
Vukan Antić <61616630+VukanAntic@users.noreply.github.com>
2025-05-18 22:45:21 +0200

    [NextImageService] init

---
+++ backend/NextImageService/src/main/java/backend/nextimageservice/NextImageServiceApplication.java
@@ -0,0 +1,13 @@
+package backend.nextimageservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NextImageServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NextImageServiceApplication.class, args);
+    }
+
+}
---
+++ backend/NextImageService/src/test/java/backend/nextimageservice/NextImageServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package backend.nextimageservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NextImageServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
