a43897137d055580a4889274fcded53309ed0376
Vukan Antić <61616630+VukanAntic@users.noreply.github.com>
2025-05-21 19:22:35 +0200

    [UserPreferenceService] Moved user deleted listener to another class

--- backend/UserPreferenceService/src/main/java/userpreferenceservice/userpreferenceservice/api/listener/UserDeletedEventHandler.java
+++ backend/UserPreferenceService/src/main/java/userpreferenceservice/userpreferenceservice/api/listener/UserDeletedEventHandler.java
@@ -1,6 +1,5 @@
 package userpreferenceservice.userpreferenceservice.api.listener;
 
-import common.common.events.UserCreatedEvent;
 import common.common.events.UserDeletedEvent;
 import lombok.AllArgsConstructor;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
@@ -9,13 +8,12 @@ import userpreferenceservice.userpreferenceservice.common.repository.UserPrefere
 
 @Component
 @AllArgsConstructor
-public class UserCreatedEventHandler {
-
+public class UserDeletedEventHandler {
     private final UserPreferenceRepository userPreferenceRepository;
 
-    @RabbitListener(queues = "${spring.rabbitmq.user_created_queue}" )
-    public void receiveUserCreatedEvent(UserCreatedEvent userCreatedEvent) {
-        System.out.println("Received event! " + userCreatedEvent);
-        userPreferenceRepository.persist(userCreatedEvent.getUsername());
+    @RabbitListener(queues = "${spring.rabbitmq.user_deleted_queue}" )
+    public void receiveUserDeletedEvent(UserDeletedEvent userDeletedEvent) {
+        System.out.println("Received event! " + userDeletedEvent);
+        userPreferenceRepository.delete(userDeletedEvent.getUsername());
     }
 }
