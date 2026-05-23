8254112cdc64859c223e5571d9eceb1768077c84
Vukan Antić <vukiantic12345@gmail.com>
2026-05-23 15:55:49 +0200

    [Client] DetailedImagePopupView init

---
+++ client/ArtOfTheDay/src/components/DetailedImagePopup/DetailedImagePopupView.tsx
@@ -0,0 +1,10 @@
+import {View} from "react-native";
+import DetailedImagePopupViewData from "@/src/components/DetailedImagePopup/DetailedImagePopupViewData";
+
+export default function DetailedImagePopupView({item}: { item: DetailedImagePopupViewData }) {
+    return (
+        <View>
+
+        </View>
+    );
+}
---
+++ client/ArtOfTheDay/src/components/DetailedImagePopup/DetailedImagePopupViewData.ts
@@ -0,0 +1,33 @@
+export default class DetailedImagePopupViewData {
+    id: string;
+    title: string;
+    year: string;
+    imageURL: string;
+    receivedAt: Date;
+    paintingDescription: string;
+    artistName: string;
+    artistLifespan: string;
+    artistDescription: string;
+
+    constructor(
+        id: string,
+        title: string,
+        year: string,
+        imageURL: string,
+        receivedAt: Date,
+        paintingDescription: string,
+        artistName: string,
+        artistLifespan: string,
+        artistDescription: string,
+    ) {
+        this.id = id;
+        this.title = title;
+        this.year = year;
+        this.imageURL = imageURL;
+        this.receivedAt = receivedAt;
+        this.paintingDescription = paintingDescription;
+        this.artistName = artistName;
+        this.artistLifespan = artistLifespan;
+        this.artistDescription = artistDescription;
+    }
+}
